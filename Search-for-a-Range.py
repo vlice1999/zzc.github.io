f2346be5ecbc5a8cf7574ca04380084874f82f95
vlice1999 <36019028+vlice1999@users.noreply.github.com>
2018-03-02 00:18:09 +0800

    Update Search-for-a-Range.py

--- Search-for-a-Range.py
+++ Search-for-a-Range.py
@@ -1,41 +1,26 @@
 class Solution(object):
-    def isValidSudoku(self, board):
+    def searchRange(self, nums, target):
         """
-        :type board: List[List[str]]
-        :rtype: bool
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
         """
-        temp1=[]
-        temp2=[]
-        for i in range(0,9):
-            temp1.append([])
-        for i in range(0,9):
-            temp2.append([])   
-        for i in range(0,9):
-            for j in range(0,9):
-                temp1[i].append(board[j][i])
-        
-        k=0
-        for i in range(0,9,3):
-            for j in range(0,9,3):
-                for row in range(i,i+3):
-                    for col in range(j,j+3):
-                        temp2[k].append(board[row][col])
-                k+=1
-        print temp2
-        
-        for i in range(0,9):
-            if self.isValid(board[i])==False or self.isValid(temp1[i])==False or self.isValid(temp2[i])==False:
-                return False
-        return True
-    
-    def isValid(self,nums):
-        temp=[]
-        for i in range(0,9):
-            if nums[i] not in temp and nums[i]!='.':
-                temp.append(nums[i])
-                continue
+        index1=0
+        index2=0
+        l=len(nums)-1
+        i=0
+        while i<l+1:
+            if nums[i]<target:
+                i+=1
             else:
-                if nums[i]=='.':
-                    continue
-                return False
-        return True
+                index1=i
+                break
+        while l>=0:
+            if nums[l]>target:
+                l-=1
+            else:
+                index2=l
+                break
+        if target not in nums:
+            return [-1,-1]
+        return [index1,index2]
